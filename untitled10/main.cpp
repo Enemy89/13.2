d0c13b389a2f69fedcd75a716dff228c32a5bd4d
Enemy89 <66246104+Enemy89@users.noreply.github.com>
2023-01-04 11:04:46 +0300

    Add files via upload

---
+++ untitled10/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector <float> prises{100.23, 24.55, 11.698, 675.5, 34.33};
+    std::vector <int> buy{1, 1, 3};
+    float sum=0;
+    for(int i = 0; i < buy.size(); ++i)
+    {
+        int x = buy[i];
+        for(int j=0; j <prises.size(); j++){
+            if (j==x)
+                sum+=prises[j];
+        }
+    }
+    std::cout<<"Sum = "<<sum;
+}
